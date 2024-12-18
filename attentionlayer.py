71754b2deb6db00e6e272b273b5dfb08616d1bb4
YOSEF MULUGETA <yosefmulugeta23@gmail.com>
2024-12-18 11:02:04 -1000

    update the attentionlayer.py

--- attentionlayer.py
+++ attentionlayer.py
@@ -5,7 +5,7 @@
     for our encoder decoder architecture
 # ==============================================================================
 """
-
+#attentionlayer
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
